d2501f6616d34d91a484b62935a1f89d9e919fe2
Jonathan Bonnin <bonninj@gmail.com>
2022-09-24 10:42:37 +0200

    initial commit with project structure

---
+++ AutomationPractice/Pages/AutomationPracticePage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationPractice.Pages
+{
+    /// <summary>
+    /// This represents an abstract page of automationpractice.com, it contains methods and fields relevant to any page.
+    /// </summary>
+    internal abstract class AutomationPracticePage
+    {
+    }
+}
